3ea3bb29ff4c3407015d8d539fdb1db5d63303b6
rafaelnunesr <rafaelrafssk@hotmail.com>
2019-03-07 13:05:22 -0300

    v01

---
+++ index.py
@@ -0,0 +1,10 @@
+def start():
+
+    def ask_and_return_seach_term():
+        return input('Type a Wikipedia term: ')
+
+    searchTerm = ask_and_return_seach_term()
+
+    print(searchTerm)
+
+start()
