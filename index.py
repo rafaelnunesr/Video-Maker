e76de6287a99d21399f6c62d7a291acaed0584b5
rafaelnunesr <rafaelrafssk@hotmail.com>
2019-03-07 13:19:11 -0300

    search term & prefix term

--- index.py
+++ index.py
@@ -3,8 +3,25 @@ def start():
     def ask_and_return_seach_term():
         return input('Type a Wikipedia term: ')
 
+    def ask_and_return_prefix():
+        prefixes = ['Who is', 'What is', 'The History of']
+
+        for i, p in enumerate(prefixes):
+            print(f'[{i+1}] -> {p}')
+
+        ask_prefix = int(input('Choose one option:'))
+
+        try:
+            return prefixes[ask_prefix-1]
+
+        except IndexError:
+            return 'Invalid option'
+
+
     searchTerm = ask_and_return_seach_term()
+    prefixTerm = ask_and_return_prefix()
 
     print(searchTerm)
+    print(prefixTerm)
 
 start()
